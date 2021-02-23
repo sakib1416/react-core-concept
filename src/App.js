d39ecfb45b74338e9cb1d3419580933a99466572
sakib1416 <sakib1416@gmail.com>
2021-02-23 17:43:02 +0100

    paragraph added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload. Ok?
         </p>
+        <p>My first react paragraph</p>
         <a
           className="App-link"
           href="https://reactjs.org"
